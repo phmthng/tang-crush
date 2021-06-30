aab62e0181aa4728d942f00337daa416d35dd1e8
phmthng <86721434+phmthng@users.noreply.github.com>
2021-06-30 18:56:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Yêu thì yêu mà không yêu thì phải yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều hihi 😘😘',
+    messDesc: 'hihi ảo thật đấy :> .',
+    btnAccept: '<333333333',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
